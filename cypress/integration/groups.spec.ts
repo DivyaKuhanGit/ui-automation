08a5198d669037f404524eb51bae994e3985e58e
Sam Melville-Coman <sam.melville-coman@smartapprentices.com>
2021-09-03 13:01:04 +0100

    find and interact with more menu on group table row

--- cypress/integration/groups.spec.ts
+++ cypress/integration/groups.spec.ts
@@ -1,44 +1,46 @@
 import { actions as primaryMenuActions } from '../domain/components/NavigationMenu.domain';
 import { actions as secondaryMenuActions } from '../domain/components/UMSideSubmenu.domain';
-import { actions as userGroupMainViewActions } from '../domain/components/UserGroups.view.domain';
+import {
+  actions as userGroupMainViewActions,
+  elements
+} from '../domain/components/UserGroups.view.domain';
 import { actions as userCreateFlowPageActions } from '../domain/components/CreateNewUserGroup.domain';
 import { actions as tenantSelectActions } from '../domain/components/TenantSelect.domain';
-import { UserGroupType } from '../domain/components/CreateNewUSerGroup.domain';
+import { UserGroupType } from '../domain/components/CreateNewUserGroup.domain';
 import { v4 } from 'uuid';
+import { findTableElementByContainingText } from '../utils/selector.util';
 
 describe('Groups: ', () => {
   beforeEach(() => {
     cy.loginTrainingProvider();
-    tenantSelectActions
-      .pickTestTenant()
-      .submitSelection();
-    primaryMenuActions
-      .clickUserManagementButton();
-    secondaryMenuActions
-      .verifyUserGroupButtonVisible()
-      .clickUserGroupButton();
+    tenantSelectActions.pickTestTenant().submitSelection();
+    primaryMenuActions.clickUserManagementButton();
+    secondaryMenuActions.verifyUserGroupButtonVisible().clickUserGroupButton();
   });
-
 
   it('SANDBOX', () => {
     //const newGroupId = `automation-${v4()}`;
-    const newGroupId = "???";
+    const newGroupId = '???';
 
     // create new group
-    userGroupMainViewActions.clickCreateGroup();
-    userCreateFlowPageActions.typyIntoNameField(newGroupId);
-    userCreateFlowPageActions.selectFromTypeDropDown(UserGroupType.EMPLOYER);
-    userCreateFlowPageActions.clickCreateNewUserGroupButton();
+    // userGroupMainViewActions.clickCreateGroup();
+    // userCreateFlowPageActions.typyIntoNameField(newGroupId);
+    // userCreateFlowPageActions.selectFromTypeDropDown(UserGroupType.EMPLOYER);
+    // userCreateFlowPageActions.clickCreateNewUserGroupButton();
 
     // verify new group in list
     primaryMenuActions.clickUserManagementButton();
     secondaryMenuActions.verifyUserGroupButtonVisible();
     secondaryMenuActions.clickUserGroupButton();
 
-    userGroupMainViewActions.getGroupByName(newGroupId)
-    .then((x) => {
-      console.log('TADA: ' + x.text());
-    });
+    elements
+      .groupTable()
+      .contains('td', newGroupId)
+      .siblings()
+      .find(`button[aria-controls*='${newGroupId}']`)
+      .click();
+    cy.focused().parent().contains('li', 'Rename group').click();
+    cy.get('[data-cy=submit-button]').click();
   });
 
   it.skip('Group permissions', () => {
