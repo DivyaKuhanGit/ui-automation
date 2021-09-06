6bf7baa9fd7aa4449d3fe6ac97855c7ba360409f
Divya_08 <divya.kunananthan@smartapprentices.com>
2021-09-06 11:00:13 +0100

    latest changes

--- cypress/domain/components/EmquiryConfigurationMenu.domain.ts
+++ cypress/domain/components/EmquiryConfigurationMenu.domain.ts
@@ -5,8 +5,11 @@ export const elementPaths={
  export const elements = {
     editEnqCloseReason: () => cy.get('[role="button"]').contains('Edit Enquiry Close Reasons'),
     addCloseReason: () => cy.get('[type="button"]').contains('Add Close Reason'),
-    enterCloseReason: () => cy.get('input[type = text]')
-};
+    enterCloseReason: () => cy.get('input[type = text]'),
+    loadMore: () => cy.get('[data-cy="enquiry-close-reasons-load-more-button"]').contains('Load more'),
+     closeReasonitems: () => cy.get('[data-cy=enquiry-close-reasons-items]').get('ul').children('.MuiListItem-container').children(),
+     childReason: () => cy.get('ul').children('.MuiListItem-container')
+ };
 
 export const actions = {
     clickEditEnqCloseReasonButton() {
@@ -18,6 +21,25 @@ export const actions = {
         elements.addCloseReason().click();
         return this;
     },
+
+    clickLoadMore() {
+        elements.loadMore().click();
+        return this;
+    },
+    verifyChildReasonVisible() {
+        elements.childReason().children();
+        return this;
+    },
+    verifycloseReasonItemsVisible() {
+        elements.closeReasonitems().should('be.visible');
+        return this;
+    },
+
+    verifyLoadMoreVisible() {
+        elements.loadMore().should('be.visible');
+        return this;
+    },
+
 
     verifyAddCloseReasonVisible() {
         elements.addCloseReason().should('be.visible');
--- cypress/integration/EnqCloseReason.spec.ts
+++ cypress/integration/EnqCloseReason.spec.ts
@@ -1,14 +1,62 @@
-import { actions as navMenuActions } from '../domain/components/NavigationMenu.domain';
+import { actions as navMenuActions, elements } from '../domain/components/NavigationMenu.domain';
 import { actions as tenantSelectActions } from '../domain/components/TenantSelect.domain';
 import { actions as bdmSumbenuActions } from '../domain/components/BdmSubmenu.domain';
 import {actions as bdmConfigMenuActions} from '../domain/components/EmquiryConfigurationMenu.domain'
-import {actions as configMenuActions} from '../domain/components/ConfigurationMenu.domain'
-import {  uuid } from 'uuidv4';
+import { actions as configMenuActions } from '../domain/components/ConfigurationMenu.domain'
+import { actions as enquiryConfigmenuActions } from '../domain/components/EmquiryConfigurationMenu.domain'
+import { uuid } from 'uuidv4';
+export function clickLiByEnumValue(cyGetElement: Function, enumStringValue: string) {
+    let elementNotFound = true;
+    cyGetElement()
+        .find('li')
+        .each(($el: { text: () => string; click: () => void; }) => {
+            if ($el.text() === enumStringValue.trim()) {
+                elementNotFound = false;
+                $el.click();
+            }
+        })
+        .then(() => {
+            if (elementNotFound) {
+                throw new Error(`Failed to find <li> element with value "${enumStringValue}"`);
+            }
+        });
+}
 
 describe('Edit Enquiry Close Reason:', () => {
   beforeEach(() => {
     cy.loginTrainingProvider();
   });
+
+  //it('BDM : Configuration Enquiries_Add Close Reason', () => {
+  //  //                  get to configuration menu
+
+  //  // select test tenant
+  //  tenantSelectActions.pickTestTenant();
+  //  tenantSelectActions.submitSelection();
+
+  //  // open BDM module
+  //  navMenuActions.verifyBuisnessDevelopmentButtonVisible();
+  //  navMenuActions.clickBuisnessDevelopmentButton();
+
+  //  // access configuration in bdm
+  //  bdmSumbenuActions.verifyConfigurationButtonVisible();
+  //  bdmSumbenuActions.clickConfigurationButton();
+
+  //  // access configure enquiries
+  //  configMenuActions.clickEnquiries();
+    
+  //  // Actual Enquiry Config screen navigation
+  //  bdmConfigMenuActions.verifyEditEnqCloseReasonVisible();
+  //  bdmConfigMenuActions.clickEditEnqCloseReasonButton();
+  //  bdmConfigMenuActions.verifyAddCloseReasonVisible();
+  //  bdmConfigMenuActions.clickAddCloseReason();
+
+  //  //Actions to be performed inside dialog box
+  //  cy.focused().get('[id="name"]').should('be.enabled').should('be.focused').type(uuid());
+  //  cy.focused().blur();
+  //  //Clicks Save button of the dialog box.
+  //  cy.get('[role="dialog"]').get('[type="submit"]').contains('Save').click(); 
+  //});
 
   it('BDM : Configuration Enquiries_Add Close Reason', () => {
     //                  get to configuration menu
@@ -38,6 +86,10 @@ describe('Edit Enquiry Close Reason:', () => {
     cy.focused().get('[id="name"]').should('be.enabled').should('be.focused').type(uuid());
     cy.focused().blur();
     //Clicks Save button of the dialog box.
-    cy.get('[role="dialog"]').get('[type="submit"]').contains('Save').click(); 
+    //cy.get('[role="dialog"]').get('[type="submit"]').contains('Save').click(); 
+    cy.get('[data-cy="cancel-button"]').click();
+
+      const orignalVal = "06871df0-35e5-4086-aeae-c75e1a42a22c";
+      enquiryConfigmenuActions.verifycloseReasonItemsVisible().contains('li, MuiListItem-container').children('.MuiListItemSecondaryAction-root').find()
   });
 });
