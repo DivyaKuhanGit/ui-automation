3f7afe2d7b3dec254b5325becadcfce9812a68dc
aleksandr.gusan <aleksandr.gusan@amdaris.com>
2021-09-09 14:10:38 +0300

    adding additional verification

--- cypress/integration/groups.spec.ts
+++ cypress/integration/groups.spec.ts
@@ -1,16 +1,16 @@
-import { actions as primaryMenuActions } from '../domain/components/NavigationMenu.domain';
-import { actions as secondaryMenuActions } from '../domain/components/UMSideSubmenu.domain';
+import { actions as primaryMenuActions } from "../domain/components/NavigationMenu.domain";
+import { actions as secondaryMenuActions } from "../domain/components/UMSideSubmenu.domain";
 import {
   actions as userGroupMainViewActions,
   elements as userGroupElements,
-  GroupItemMenuActions
-} from '../domain/components/UserGroups.view.domain';
-import { actions as userCreateFlowPageActions } from '../domain/components/CreateNewUserGroup.domain';
-import { actions as tenantSelectActions } from '../domain/components/TenantSelect.domain';
-import { UserGroupType } from '../domain/components/CreateNewUserGroup.domain';
-import { v4 } from 'uuid';
+  GroupItemMenuActions,
+} from "../domain/components/UserGroups.view.domain";
+import { actions as userCreateFlowPageActions } from "../domain/components/CreateNewUserGroup.domain";
+import { actions as tenantSelectActions } from "../domain/components/TenantSelect.domain";
+import { UserGroupType } from "../domain/components/CreateNewUserGroup.domain";
+import { v4 } from "uuid";
 
-describe('Groups: ', () => {
+describe("Groups: ", () => {
   beforeEach(() => {
     cy.loginTrainingProvider();
     tenantSelectActions.pickTestTenant().submitSelection();
@@ -19,7 +19,7 @@ describe('Groups: ', () => {
 
   // TODO: this test is overloaded, need to think of a good way
   //  to break it up into separate testable actions
-  it('Group permissions', () => {
+  it("Group permissions", () => {
     let newGroupName = `automation-${v4()}`;
 
     // create new group
@@ -33,45 +33,48 @@ describe('Groups: ', () => {
 
     searchForGroupByName(newGroupName);
 
+    // verify only 1 item in table afte filtering via search
+    // +1 because table header is also a <tr>
+    userGroupElements.groupTable().find("tr").should("have.length", 2);
+
     // get into items menu by containing text
     userGroupElements
       .groupTable()
-      .contains('td', newGroupName)
+      .contains("td", newGroupName)
       .siblings()
       .find(`button[aria-controls*='${newGroupName}']`)
       .click();
 
     // rename item
-    cy.focused().contains('li', GroupItemMenuActions.RENAME).click();
-    cy.focused().type('-updated');
-    newGroupName += '-updated';
-    cy.get('[data-cy=submit-button]').click();
+    cy.focused().contains("li", GroupItemMenuActions.RENAME).click();
+    cy.focused().type("-updated");
+    newGroupName += "-updated";
+    cy.get("[data-cy=submit-button]").click();
 
     searchForGroupByName(newGroupName);
 
     // get into items menu by containing text
     userGroupElements
       .groupTable()
-      .contains('td', newGroupName)
+      .contains("td", newGroupName)
       .siblings()
       .find(`button[aria-controls*='${newGroupName}']`)
       .click();
 
     // click delete group
     cy.focused()
-      .contains('li', GroupItemMenuActions.RENAME)
+      .contains("li", GroupItemMenuActions.RENAME)
       .siblings()
-      .contains('li', GroupItemMenuActions.DELETE_GROUP)
+      .contains("li", GroupItemMenuActions.DELETE_GROUP)
       .click();
 
     // confirm delete
-    cy.get('[role="dialog"]').contains('span', 'Confirm').click();
+    cy.get('[role="dialog"]').contains("span", "Confirm").click();
 
     navigateToGroupLisitingPage();
     searchForGroupByName(newGroupName);
 
-    cy.get('.MuiAutocomplete-noOptions')
-    .should('contain.text', 'No options');
+    cy.get(".MuiAutocomplete-noOptions").should("contain.text", "No options");
   });
 });
 
@@ -85,5 +88,5 @@ export function searchForGroupByName(groupName: string) {
   userGroupElements.searchBox().type(`${groupName}`).click();
   // this is flaky
   cy.wait(1000);
-  userGroupElements.searchBox().type('{enter}');
+  userGroupElements.searchBox().type("{enter}");
 }
