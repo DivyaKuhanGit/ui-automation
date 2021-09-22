54ffa7c2b5bb91318f35edb931ef60c529146af3
Divya_08 <divya.kunananthan@smartapprentices.com>
2021-09-22 20:33:52 +0100

    Add New Enquiry Type

--- cypress/integration/EnquiryType.spec.ts
+++ cypress/integration/EnquiryType.spec.ts
@@ -1,7 +1,6 @@
 import { actions as navMenuActions } from '../domain/components/NavigationMenu.domain';
 import { actions as tenantSelectActions } from '../domain/components/TenantSelect.domain';
 import { actions as bdmSumbenuActions } from '../domain/components/BdmSubmenu.domain';
-import { actions as bdmConfigMenuActions } from '../domain/components/EmquiryConfigurationMenu.domain';
 import { actions as configMenuActions } from '../domain/components/ConfigurationMenu.domain';
 import { actions as enquiryConfigmenuactions } from '../domain/components/EmquiryConfigurationMenu.domain';
 import { elements as enquiryConfigElements } from '../domain/components/EmquiryConfigurationMenu.domain';
