bfcd2f8e172ee41614ea880fc71aea59a8b1921b
Divya_08 <divya.kunananthan@smartapprentices.com>
2021-09-28 18:30:51 +0100

    Enquiry Type Rename

--- cypress/integration/EnquiryType.spec.ts
+++ cypress/integration/EnquiryType.spec.ts
@@ -5,15 +5,11 @@ import { actions as configMenuActions } from '../domain/components/Configuration
 import { actions as enquiryConfigmenuactions } from '../domain/components/EmquiryConfigurationMenu.domain';
 import { elements as enquiryConfigElements } from '../domain/components/EmquiryConfigurationMenu.domain';
 import { v4 } from 'uuid';
-const randomVal = v4();
 const enquiryTypeTable = '[data-cy=enquiry-types-table]';
 
 describe("Enquiry Type : ", () => {
     beforeEach(() => {
         cy.loginTrainingProvider();
-    });
-
-    it("BDM Add New Enquiry Type", () => {
         // select test tenant
         tenantSelectActions.pickTestTenant();
         tenantSelectActions.submitSelection();
@@ -23,6 +19,11 @@ describe("Enquiry Type : ", () => {
 
         // access configuration in bdm
         bdmSumbenuActions.clickConfigurationButton();
+    });
+
+    it("BDM Add New Enquiry Type", () => {
+        const randomVal = v4();
+        const orignalType = randomVal;
 
         // access configure enquiries
         configMenuActions.clickEnquiries();
@@ -34,6 +35,39 @@ describe("Enquiry Type : ", () => {
         cy.focused().get('[id="name"]').should('be.enabled').should('be.focused').type(randomVal);
         cy.focused().blur();
         enquiryConfigElements.addEnqSavebtn().click();
-        cy.get(enquiryTypeTable).contains('p',randomVal).should('be.visible');
+
+        cy.get(enquiryTypeTable).contains('p', orignalType);
+    });
+
+    it("BDM Rename Enquiry Type", () => {
+        const nameVal = v4();
+        const renameVal = nameVal;
+
+        // access configure enquiries
+        configMenuActions.clickEnquiries();
+
+        //Actual Enquiry Type Screen Navigation
+        enquiryConfigmenuactions.clickAddNewEnqType();
+
+        //Actions to be performed inside dialog box
+        cy.focused().get('[id="name"]').should('be.enabled').should('be.focused').type(nameVal);
+        cy.focused().blur();
+        enquiryConfigElements.addEnqSavebtn().click();
+
+        //Actions to be performed to find and rename the added value.
+        
+        cy.get(enquiryTypeTable)
+            .contains('p', renameVal)
+            .parent()
+            .siblings()
+            .children('button')
+            .click();
+
+        cy.get('li[data-cy*="rename-button"]').focused().click();
+
+        //Renaming the Old Reason Name to New Name
+        cy.get('[role=dialog]').get('[data-cy="name-field"]').clear();
+        enquiryConfigElements.renameTextfield().type('Renamed-' + v4());
+        enquiryConfigElements.addEnqSavebtn().click();
     });
 });
