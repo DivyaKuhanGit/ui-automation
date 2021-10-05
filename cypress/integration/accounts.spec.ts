bff796f511b31197c4042d6a407eb3ead44c4e65
aleksandr.gusan <aleksandr.gusan@amdaris.com>
2021-10-05 12:21:02 +0300

    wip

--- cypress/integration/accounts.spec.ts
+++ cypress/integration/accounts.spec.ts
@@ -1,6 +1,15 @@
 import { actions as tenantSelectActions } from "../domain/components/TenantSelect.domain";
 import { actions as primaryMenuActions } from "../domain/components/NavigationMenu.domain";
 import { actions as secondaryMenuActions } from "../domain/components/BdmSubmenu.domain";
+import { v4 as uuid } from "uuid";
+
+const addAccountButton = () => cy.get("[data-cy=add-account-button]");
+const newAccountNameField = () => cy.get("#name");
+const newAccountModalSubmit = () => cy.get("[data-cy=submit-button]");
+const newAccountModalCancel = () => cy.get("[data-cy=cancel-button]");
+const searchForAccountTextFiled = () =>
+  cy.get("[data-testid=SearchIcon]").siblings();//.contains("input");
+const accountsTableView = () => cy.get('[aria-label="accounts-table"]');
 
 describe("Accounts: ", () => {
   beforeEach(() => {
@@ -10,8 +19,22 @@ describe("Accounts: ", () => {
   });
 
   it("Create Account", () => {
-    cy.get('[aria-label="accounts-table"]').contains('tr','24')
+    const newAccountName = uuid();
+
+    addAccountButton().click();
+    newAccountNameField().type(newAccountName);
+    newAccountModalSubmit().click();
+
+    // FIXME: BUG here
+    newAccountModalCancel().click();
+
+    searchForAccountTextFiled().type(newAccountName);
+    accountsTableView().contains('tr',newAccountName).contains('[data-testid=MoreVertOutlinedIcon]').click();
   });
+
+  // it("Create Account", () => {
+  //   cy.get('[aria-label="accounts-table"]').contains('tr','24')
+  // });
 });
 
 export function navigateToAccountsPage() {
