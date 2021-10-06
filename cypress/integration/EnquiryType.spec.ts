d31ac23adacab7aeedd1cd13726cad05c424e74c
Divya_08 <divya.kunananthan@smartapprentices.com>
2021-10-06 19:10:54 +0100

    Resolved comments

--- cypress/integration/EnquiryType.spec.ts
+++ cypress/integration/EnquiryType.spec.ts
@@ -21,67 +21,67 @@ describe("Enquiry Type : ", () => {
         bdmSumbenuActions.clickConfigurationButton();
     });
 
-    //it("BDM Add New Enquiry Type", () => {
-    //    const randomVal = v4();
-    //    const orignalType = randomVal;
+    it("BDM Add New Enquiry Type", () => {
+        const randomVal = v4();
+        const orignalType = randomVal;
 
-    //    // access configure enquiries
-    //    configMenuActions.clickEnquiries();
+        // access configure enquiries
+        configMenuActions.clickEnquiries();
 
-    //    //Actual Enquiry Type Screen Navigation
-    //    enquiryConfigmenuactions.clickAddNewEnqType();
+        //Actual Enquiry Type Screen Navigation
+        enquiryConfigmenuactions.clickAddNewEnqType();
 
-    //    //Actions to be performed inside dialog box
-    //    cy.focused().get('[id="name"]').should('be.enabled').should('be.focused').type(randomVal);
-    //    cy.focused().blur();
-    //    enquiryConfigElements.addEnqSaveBtn().click();
-    //    enquiryConfigmenuactions.clickLoadAllPages();
-    //    enquiryConfigmenuactions.clickAllPages();
-    //    enquiryConfigElements.enquiryTypeTable().contains('p', orignalType);
-    //});
+        //Actions to be performed inside dialog box
+        cy.focused().get('[id="name"]').should('be.enabled').should('be.focused').type(randomVal);
+        cy.focused().blur();
+        enquiryConfigElements.addEnqSaveBtn().click();
+        enquiryConfigmenuactions.clickLoadAllPages();
+        enquiryConfigmenuactions.clickAllPages();
+        enquiryConfigElements.enquiryTypeTable().contains('p', orignalType);
+    });
 
-    //it("BDM Rename Enquiry Type", () => {
-    //    const nameVal = v4();
-    //    const renameVal = nameVal;
-    //    const typeName = v4();
-    //    const checkname = 'Renamed-' + typeName;
+    it("BDM Rename Enquiry Type", () => {
+        const nameVal = v4();
+        const renameVal = nameVal;
+        const typeName = v4();
+        const checkname = 'Renamed-' + typeName;
 
-    //    // access configure enquiries
-    //    configMenuActions.clickEnquiries();
+        // access configure enquiries
+        configMenuActions.clickEnquiries();
 
-    //    //Actual Enquiry Type Screen Navigation
-    //    enquiryConfigmenuactions.clickAddNewEnqType();
+        //Actual Enquiry Type Screen Navigation
+        enquiryConfigmenuactions.clickAddNewEnqType();
 
-    //    //Actions to be performed inside dialog box
-    //    cy.focused().get('[id="name"]').should('be.enabled').should('be.focused').type(nameVal);
-    //    cy.focused().blur();
-    //    enquiryConfigElements.addEnqSaveBtn().click();
+        //Actions to be performed inside dialog box
+        cy.focused().get('[id="name"]').should('be.enabled').should('be.focused').type(nameVal);
+        cy.focused().blur();
+        enquiryConfigElements.addEnqSaveBtn().click();
 
-    //    //Actions to be performed to find and rename the added value.
-    //    enquiryConfigmenuactions.clickLoadAllPages();
-    //    enquiryConfigmenuactions.clickAllPages();
-    //    enquiryConfigElements.enquiryTypeTable()
-    //        .contains('p', renameVal)
-    //        .parent()
-    //        .siblings()
-    //        .children('button')
-    //        .click();
+        //Actions to be performed to find and rename the added value.
+        enquiryConfigmenuactions.clickLoadAllPages();
+        enquiryConfigmenuactions.clickAllPages();
+        enquiryConfigElements.enquiryTypeTable()
+            .contains('p', renameVal)
+            .parent()
+            .siblings()
+            .children('button')
+            .click();
 
-    //    cy.get('li[data-cy*="rename-button"]').focused().click();
+        cy.get('li[data-cy*="rename-button"]').focused().click();
 
-    //    //Renaming the Old Reason Name to New Name
-    //    cy.get('[role=dialog]').get('[data-cy="name-field"]').clear();
-    //    enquiryConfigElements.renameTextfield().type(checkname);
-    //    enquiryConfigElements.addEnqSaveBtn().click();
-    //    enquiryConfigmenuactions.clickLoadAllPages();
-    //    enquiryConfigmenuactions.clickAllPages();
-    //    enquiryConfigElements.enquiryTypeTable().contains('p', checkname);
-    //});
+        //Renaming the Old Reason Name to New Name
+        cy.get('[role=dialog]').get('[data-cy="name-field"]').clear();
+        enquiryConfigElements.renameTextfield().type(checkname);
+        enquiryConfigElements.addEnqSaveBtn().click();
+        enquiryConfigmenuactions.clickLoadAllPages();
+        enquiryConfigmenuactions.clickAllPages();
+        enquiryConfigElements.enquiryTypeTable().contains('p', checkname);
+    });
 
     it("BDM : Create Enquiry Type and Set Enquiry Status", () => {
-        const actualVal = "99d68d7b-596e-4cea-96c7-86a51c13d181";//v4();
+        const actualVal = v4();
         const editedVal = actualVal;
-        const statusVal = "9cfcea02-4eb0-4108-bf72-1487bdd16dd1";//v4();
+        const statusVal = v4();
         const refStatus = statusVal;
 
         //Actions to get response after loading all pages
@@ -97,12 +97,12 @@ describe("Enquiry Type : ", () => {
 
         //Actions to Add New Enquiry Status
         enquiryConfigmenuactions.clickEditEnqStatus();
-        //enquiryConfigmenuactions.clickAddEnqStatus();
+        enquiryConfigmenuactions.clickAddEnqStatus();
 
-        ////Actions to be performed inside dialog box
-        //cy.focused().get('[role="dialog"]').get('[data-cy="name-field"]').focused().type(statusVal);
-        //cy.focused().blur();
-        //enquiryConfigElements.addEnqSaveBtn().click();
+        //Actions to be performed inside dialog box
+        cy.focused().get('[role="dialog"]').get('[data-cy="name-field"]').focused().type(statusVal);
+        cy.focused().blur();
+        enquiryConfigElements.addEnqSaveBtn().click();
 
         // wait for the request to the api to get the first page of enquiry close reasons
         cy.wait('@resp').then((r) => {
@@ -117,13 +117,13 @@ describe("Enquiry Type : ", () => {
         // access configure enquiries
         configMenuActions.clickEnquiries();
 
-        ////Actual Enquiry Type Screen Navigation
-        //enquiryConfigmenuactions.clickAddNewEnqType();
+        //Actual Enquiry Type Screen Navigation
+        enquiryConfigmenuactions.clickAddNewEnqType();
 
-        ////Actions to be performed inside dialog box
-        //cy.focused().get('[id="name"]').should('be.enabled').should('be.focused').type(actualVal);
-        //cy.focused().blur();
-        //enquiryConfigElements.addEnqSaveBtn().click();
+        //Actions to be performed inside dialog box
+        cy.focused().get('[id="name"]').should('be.enabled').should('be.focused').type(actualVal);
+        cy.focused().blur();
+        enquiryConfigElements.addEnqSaveBtn().click();
 
         //Actions to click Action menu and Edit Button
         enquiryConfigmenuactions.clickLoadAllPages();
@@ -140,28 +140,24 @@ describe("Enquiry Type : ", () => {
             .click();
 
         //Actions to click Enquiry Status Dropdown box.
-        // enquiryConfigElements.enquiryTypeStatus()
-        //     .children()
-        //     .children()
-        //     .children()
-        //     .children('button')
-        //     .click()
-            // .focused()
-            // .contains('refStatus').check({ force: true })
-        //cy.scrollTo('bottom', { ensureScrollable: false })
+         enquiryConfigElements.enquiryTypeStatus()
+             .children()
+             .children()
+             .children()
+             .children('button')
+             .click()
 
-        ////Type and checks the required EnquiryStatus
-        //cy.focused()
-        //enquiryConfigElements.enquiryTypeStatus().type(refStatus)
-        //cy.get('[type="checkbox"]').check()
-        //    .focused()
-        //    .blur();
+        //Type and checks the required EnquiryStatus in dropdown box
+        cy.focused()
+        enquiryConfigElements.enquiryTypeStatus()
+            .type('{downarrow}')
+            .type('{end}')
+            .type(refStatus)
 
-        //cy.get('data-cy="enquiry-type-statuses"').contains('li',refStatus).click();
-        //enquiryConfigElements.enquiryTypeStatus().contains('li',refStatus);
-
-        enquiryConfigElements.enquiryTypeStatus().type('-/-');
-        cy.focused().type('{downarrow}').type('{enter}');
+        cy.get('[type="checkbox"]').not('disabled')
+            .check().should('be.checked')
+            .focused()
+            .blur();
     });
 });
 
